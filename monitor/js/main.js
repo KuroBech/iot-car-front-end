faa40012dd4ac317d09be67aba3e7b9b6c9e6ac0
KuroBech <60513501+KuroBech@users.noreply.github.com>
2024-05-23 11:36:52 -0600

    Update main.js

--- monitor/js/main.js
+++ monitor/js/main.js
@@ -7,7 +7,7 @@ let mensaje = document.getElementById('mensaje');
 
 function callAPI(valor){
     // Hacer una petición para un usuario con ID especifico
-    axios.get('http://localhost/iot-car-control/backend/apis/getRegistro.php')
+    axios.get('http://18.170.71.79/iot-car-control/backend/apis/getRegistro.php')
       .then(function (response) {
         // manejar respuesta exitosa
         console.log(response);
